df017b7adc7ed774baf934c3f8f7e8527d808ef0
Chenda Zhang <chenda1997@hotmail.com>
2025-03-14 16:19:02 -0700

    Update cpu_load_2.sh

--- cpu_load_2.sh
+++ cpu_load_2.sh
@@ -7,18 +7,6 @@
 WORKERS=2
 DURATION=5000
 
-# If you want to pass custom arguments, you could parse them here.
-# Example usage:
-#   ./run_cpu_stress.sh 4 60
-
-if [[ -n "$1" ]]; then
-  WORKERS=$1
-fi
-
-if [[ -n "$2" ]]; then
-  DURATION=$2
-fi
-
 echo "Running cpu_load.py with $WORKERS workers for $DURATION seconds."
 
 # Adjust path if necessary.
